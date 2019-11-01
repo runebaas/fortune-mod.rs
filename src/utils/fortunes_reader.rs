f88979e95ae45e41c83db14ec55dd54bec3a48b2
runebaas <runebaas@gmail.com>
2019-11-01 13:28:47 +0100

    Add an additional fortune file lookup path for unix systems (so it works on arch linux)

--- src/utils/fortunes_reader.rs
+++ src/utils/fortunes_reader.rs
@@ -51,7 +51,7 @@ fn get_paths() -> Vec<String> {
 
 #[cfg(unix)]
 fn get_paths() -> Vec<String> {
-    own_vec(vec!["./fortunes", "./cookies", "/usr/share/games/fortunes"])
+    own_vec(vec!["./fortunes", "/usr/share/fortune", "/usr/share/games/fortunes"])
 }
 
 pub struct Cookie {
